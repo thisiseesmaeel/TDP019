ca79a70bd5ffcf2ddb953cbf095c62026bb8c1ae
Ahmed Sikh <ahmsi881@student.liu.se>
2021-05-12 17:12:23 +0200

    asd

---
+++ Doc/redö/ETL-redo/ETL.rb
@@ -0,0 +1,233 @@
+##Alla tokens, matchregler och matchgrupper
+
+require './rdparse.rb'
+require './classes.rb'
+
+class Etl
+    attr_accessor :output
+    def initialize
+        @etlParser = Parser.new("ETL") do
+      #+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+- BEGIN TOKENS +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-
+        token(/\<comment[^!]*\<end/) #parsa inte och ignorera flerradskommentarer
+        token(/(<<.+$)/) #parsa inte och ignorera enradskommentar
+        token(/\s+/)  #mellanrum ska inte parsas och ignoreras
+        token(/(\d+[.]\d+)/) { |m| m.to_f } #floattal
+        token(/\d+/) { |m| m.to_i } #heltal
+        token(/'[^\']*'/) { |m| m } #sträng inom enkeltcitattecken (' ')
+        token(/"[^\"]*"/) { |m| m } #sträng inom dubbeltcitattecken (" ")
+        token(/[a-z]+[a-z0-9_]*/) { |m| m } #namn på variabler
+        token(/./) { |m| m } #allt annat(enkla käraktarer)
+      #+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+- END TOKENS +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-
+      
+      #+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+- BEGIN BNF +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+ 
+        start :program do
+            match(:statements)
+            end
+ 
+        rule :statements do
+            match(:statements, :statement){ |states, state| [states, state].flatten }
+            match(:statement)
+        end 
+
+        rule :statement do
+            match(:return)
+            match(:func) 
+            match(:funcCall)
+            match(:stop) 
+            match(:print)
+            match(:if_box) 
+            match(:whileIteration)
+            match(:assign)
+            end
+
+        rule :assign do
+            match(:id, "=", :bool_logic) { |variable_name, _, bool_log| Assign.new(variable_name, bool_log) }
+            match(:id, "=", :multiple_strings) { |variable_name, _, mult_str| Assign.new(variable_name, mult_str) }
+            match(:id, "=", :string_expr) { |variable_name, _, str_exp| Assign.new(variable_name, str_exp) }
+            match(:id, "=", :expr) { |variable_name, _, expr| Assign.new(variable_name, expr) }
+            end
+
+        rule :string_expr do
+            match(/'[^\']*'/) { |string| Constant.new(string[1, string.length-2]) }
+            match(/"[^\"]*"/) { |string| Constant.new(string[1, string.length-2]) }
+            end  
+
+        rule :multiple_strings do
+            match(:string_expr, "plus", :string_expr) { |str_exp1, _, str_exp2| Plus_str.new("plus", str_exp1, str_exp2) }
+            match(:multiple_strings, "plus", :string_expr) { |mult_str, _, str_exp| Plus_str.new("plus", mult_str, str_exp) }
+            match(:id, "plus", :id) { |id1, _, id2| Plus_str.new("plus", id1, id2) }
+            match(:multiple_strings, "plus", :id) { |mult_str, _, id| Plus_str.new("plus", mult_str, id) }
+            end 
+        
+        rule :expr do
+            match(:expr, '+', :term) { |expr, _, term| Expr.new('+', expr, term) }
+            match(:expr, '-', :term) { |expr, _, term| Expr.new('-', expr, term) }
+            match(:term)
+            end
+
+        rule :term do
+            match(:term, '*', :atom) { |term, _, atom| Expr.new('*', term, atom) }
+            match(:term, '/', :atom) { |term, _, atom| Expr.new('/', term, atom) }
+            match(:term, '^', :atom) { |term, _, atom| Expr.new('^', term, atom) }
+            match(:term, '%', :atom) { |term, _, atom| Expr.new('%', term, atom) }
+            match(:atom)
+            end
+            
+       rule :bool_logic do
+            match(:bool_logic, 'and', :bool_logic) { |lhs, _, rhs| Condition.new('and', lhs, rhs) }
+            match(:bool_logic, 'or', :bool_logic) { |lhs, _, rhs| Condition.new('or', lhs, rhs) }
+            match('not', :bool_logic) { |_, oper| Not.new('not', oper) }
+            match('true') { Constant.new(true) }
+            match('false') { Constant.new(false) }
+            match('(', :bool_logic, ')') { |_, bool_log, _| bool_log }
+            match(:bool_list)
+            end 
+
+        rule :bool_list do
+            match(:less_than)
+            match(:greater_than)
+            match(:less_than_or_equal_to)
+            match(:greater_than_or_equal_to)
+            match(:not_equal_to)
+            match(:equal)
+        end
+
+        rule :less_than do
+            match(:expr, '<', :expr) { |expr1, _, expr2| Condition.new('<', expr1, expr2) }
+            match(:expr, 'less', 'than', :expr) { |expr1, _, _, expr2| Condition.new('less than', expr1, expr2) }
+        end
+
+        rule :greater_than do
+            match(:expr, '>', :expr) { |expr1, _, expr2| Condition.new('>', expr1, expr2) }
+            match(:expr, 'greater', 'than', :expr) { |expr1, _, _, expr2| Condition.new('greater than', expr1, expr2) }
+        end
+
+        rule :less_than_or_equal_to do
+            match(:expr, '<', '=', :expr) { |expr1, _, _, expr2| Condition.new('<=', expr1, expr2) }
+            match(:expr, 'less', 'than', 'or', 'equal', 'to', :expr) { |expr1, _, _, _, _, _, expr2| Condition.new('less than or equal to', expr1, expr2) }
+        end
+
+        rule :greater_than_or_equal_to do
+            match(:expr, '>', '=', :expr) { |expr1, _, _, expr2| Condition.new('>=', expr1, expr2) }
+            match(:expr, 'greater', 'than', 'or', 'equal', 'to', :expr) { |expr1, _, _, _, _, _, expr2| Condition.new('greater than or equal to', expr1, expr2) }
+        end
+
+        rule :not_equal_to do
+            match(:expr, '!', '=', :expr) { |expr1,_, _, expr2| Condition.new('!=', expr1, expr2) }
+            match(:expr, 'not', 'equal', 'to', :expr) { |expr1, _, _, _, expr2| Condition.new('not equal to', expr1, expr2) }
+        end
+
+        rule :equal do
+            match(:expr, '=', '=', :expr) { |expr1,_, _, expr2| Condition.new('==', expr1, expr2) }
+            match(:expr, 'equal', :expr) { |expr1, _, expr2| Condition.new('equal', expr1, expr2) }
+        end
+
+        rule :id do
+            match(/[a-z]+[a-z0-9_]*/) { |id| Variable.new(id) }
+            end
+
+        rule :func do
+            match("define", /[a-z]+[a-z0-9_]*/, "(", :arguments, ")", :statements, "enddef") { |_, def_name, _, args, _, states, _| 
+                Function.new(def_name, args, states) }
+            match("define", /[a-z]+[a-z0-9_]*/, "(", ")", :statements, "enddef") { |_, def_name, _, _, states, _| Function.new(def_name, Array.new, states) }
+            end
+
+        rule :funcCall do
+            match(:id, "(", ")") { |def_name, _, _| FunctionCall.new(def_name, Array.new) }
+            match(:id, "(", :arguments, ")") { |def_name, _, args, _| FunctionCall.new(def_name, args) }
+            end 
+
+        rule :return do
+            match("return", :argument) { |_, arg| Return.new(arg) }
+            end
+
+        rule :arguments do
+            match(:arguments, ',', :argument){ |args,_,arg| [args, arg].flatten }
+            match(:argument)
+            end  
+
+        rule :argument do
+            match(:string_expr)
+            match(:expr)
+            end  
+
+        rule :whileIteration do
+            match("while", "(", :bool_logic, ")", :statements, "endwhile") { |_, _, bool_log, _, states, _| While.new(bool_log, states) }    
+            end
+
+        rule :stop do
+            match("stop") { |_| Stop.new() }
+            end
+    
+        rule :if_box do
+            match("if", "(", :bool_logic, ")", "then", :statements, "endif") { |_, _, bool_log, _, _, if_states, _| If.new(bool_log, if_states) }
+            match("if", "(", :bool_logic, ")", "then", :statements, "otherwise", :statements, "endif") { |_, _, bool_log, _, _, if_states, _, else_states, _| 
+                If.new(bool_log, if_states, else_states) }
+            match("if", "(", :bool_logic, ")", "then", :statements, "elseif", "(", :bool_logic, ")", "then", :statements, "otherwise", :statements, "endif") { |_, _, bool_log, _, _, if_states, _, _, elsif_bool_log, _, _, elsif_state, _, else_states, _| 
+                If.new(bool_log, if_states, elsif_bool_log, elsif_state, else_states) }
+            end    
+
+        rule :print do
+            match("write", :multiple_strings) { |_, mult_str| Print.new(mult_str) }
+            match("write", :string_expr) { |_, str_exp| Print.new(str_exp) }
+            match("write", :bool_logic) { |_, bool_log| Print.new(bool_log) } 
+            match("write", :expr) { |_, exp| Print.new(exp) } 
+            end
+
+        rule :atom do
+            match(:funcCall)
+            match(Float) { |float_num| Constant.new(float_num) }
+            match(Integer) { |int_num| Constant.new(int_num) }
+            match("-", Float) { |a, b| Constant.new(b, a) }
+            match("-", Integer) { |a, b| Constant.new(b, a) }
+            match('(', :expr, ')') { |_,exp,_| Expression.new(exp) }
+            match(:id)
+            end
+            
+        end #end för all grammatik
+    #+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+- END BNF +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+    end #end för initialize  
+      
+    def done(str)
+        ["quit", "exit", "bye", "close", "stop"].include?(str.chomp)
+    end
+
+    #För att starta programmet i terminalen
+    def activate_terminal
+        print "[ETL] "
+        str = gets
+        if done(str) then
+            puts "Bye."
+        else
+            parsePrinter = @etlParser.parse str
+            puts "=> #{parsePrinter.eval}"
+            activate_terminal
+        end
+    end
+
+    #För att testa från en fil
+    def activate_file(etl_file)
+        @output = []
+        etl_file = File.read(etl_file)
+        @output = @etlParser.parse(etl_file)
+        @output
+    end
+    
+    def log(state = true)
+        if state
+          @etlParser.logger.level = Logger::DEBUG
+        else
+          @etlParser.logger.level = Logger::WARN
+        end
+    end
+
+end #end för klassen
+
+checkEtl = Etl.new
+checkEtl.log(false)
+#checkEtl.activate_terminal
+checkEtl.activate_file("etl.etl")
+checkEtl.output.each { |segment|
+	if segment.class != Function and segment.class != FunctionCall
+		 segment.eval()
+    end }
---
+++ Doc/redö/ETL-redo/classes.rb
@@ -0,0 +1,321 @@
+## Alla klasser som behövs
+
+$our_funcs = Hash.new
+
+class ScopeHandler
+    def initialize()
+        @@level = 1
+        @@holder = {}
+    end
+    def defineScope(s)
+        @@holder = s
+        return @@holder
+    end
+    def receiveHolder()
+        return @@holder
+    end
+    def receiveLevel()
+        return @@level
+    end
+    def incre()
+        @@level = @@level + 1
+        return @@holder
+    end
+    def decre(s)
+        defineScope(s)
+        @@level = @@level - 1
+        return nil
+    end
+end
+
+$scope = ScopeHandler.new
+
+def look_up(variable, our_vars)
+    levelNr = $scope.receiveLevel
+    if our_vars == $scope.receiveHolder
+        loop do 
+            if our_vars[levelNr] != nil and our_vars[levelNr][variable] != nil
+                return our_vars[levelNr][variable]
+            end
+            levelNr = levelNr - 1
+        break if (levelNr < 0)
+        end
+        
+        if our_vars[levelNr] == nil
+            our_vars[variable]
+        end
+    end
+end
+
+class Variable
+    attr_accessor :variable_name
+    def initialize(id)
+        @variable_name = id
+    end
+    def eval
+        return look_up(@variable_name, $scope.receiveHolder)
+    end
+end
+
+class Expr
+    attr_accessor :sign, :lhs, :rhs
+    def initialize(sign, lhs, rhs)
+        @sign = sign
+        @lhs = lhs
+        @rhs = rhs
+    end
+    def eval()
+        case sign
+            when '+'
+                return lhs.eval + rhs.eval 
+            when '-'
+                return lhs.eval - rhs.eval
+            when '*'
+                return lhs.eval * rhs.eval 
+            when '/'
+                return lhs.eval / rhs.eval
+            when '^'
+                return lhs.eval ** rhs.eval
+            when '%'
+                return lhs.eval % rhs.eval
+            else nil
+        end
+    end
+end
+
+class Plus_str
+    attr_accessor :sign, :lhs, :rhs
+    def initialize(sign, lhs, rhs)
+        @sign = sign
+        @lhs = lhs
+        @rhs = rhs
+    end
+    def eval()
+        case @sign
+            when 'plus'
+                return @lhs.eval + @rhs.eval 
+            else nil
+        end
+    end
+end
+
+class Condition
+    attr_accessor :sign, :lhs, :rhs
+    def initialize(sign, lhs, rhs)
+        @sign = sign
+        @lhs = lhs
+        @rhs = rhs
+    end
+    def eval()
+        case sign
+            when '<', 'less than'
+                return lhs.eval < rhs.eval 
+            when '>', 'greater than'
+                return lhs.eval > rhs.eval 
+            when '<=', 'less than or equal to'
+                return lhs.eval <= rhs.eval 
+            when '>=', 'greater than or equal to'
+                return lhs.eval >= rhs.eval 
+            when '!=', 'not equal to'
+                return lhs.eval != rhs.eval
+            when '==', 'equal'
+                return lhs.eval == rhs.eval
+            when 'and'
+                return lhs.eval && rhs.eval
+            when 'or'
+                return lhs.eval || rhs.eval
+            else nil
+        end
+    end
+end
+
+class Not
+    attr_accessor :sign, :oper
+    def initialize(sign, oper)
+        @sign = sign
+        @oper = oper
+    end
+    def eval()
+        case sign
+            when 'not'
+                return  (not oper.eval)
+            else nil
+        end
+    end
+end
+
+class Expression
+    def initialize(value)
+        @value = value
+    end
+    def eval()
+        @value.eval
+    end
+end
+
+class Assign
+    attr_reader :variable, :assign_expr
+    def initialize(variable, assign_expr)
+        @variable = variable
+        @assign_expr = assign_expr
+    end
+    def eval
+        value = @assign_expr.eval
+        @level_Nr = $scope.receiveLevel
+        scp = $scope.receiveHolder
+        if scp[@level_Nr] != nil
+            if scp[@level_Nr].has_key?(@variable.variable_name)
+                return scp[@level_Nr][@variable.variable_name] = value
+            else
+                scp[@level_Nr][@variable.variable_name] = value
+                return $scope.defineScope(scp)
+            end
+        elsif scp[@level_Nr] = {} and scp[@level_Nr][@variable.variable_name] = value
+            return $scope.defineScope(scp)
+        end
+    end
+end
+
+class Constant
+    attr_accessor :value
+    def initialize (value, negative = nil)
+        @value = value
+        @negative = negative
+    end
+    def eval()
+        if @negative
+            @value * -1
+        else
+            @value
+        end
+    end
+end 
+
+class Print
+    def initialize(value)
+        @value = value
+    end
+    def eval()
+        #puts
+        if @value.eval != nil   
+            puts "-->> Printing '#{@value.eval}'"
+            @value.eval
+        else
+            nil
+        end   
+    end
+end
+
+class If
+    attr_accessor :if_bool_logic, :states, :elsif_bool_logic, :elsif_state, :otherwise_states
+    def initialize(if_bool_logic, states, elsif_bool_logic = nil, elsif_state = nil, otherwise_states = nil)
+        @if_bool_logic = if_bool_logic
+        @states = states
+        @elsif_bool_logic = elsif_bool_logic
+        @elsif_state = elsif_state
+        @otherwise_states = otherwise_states
+    end
+    def eval()
+        if @if_bool_logic.eval()
+            @states.eval()
+        elsif @elsif_bool_logic.eval()
+                @elsif_state.eval()
+        elsif @otherwise_states != nil
+            @otherwise_states.eval()
+        end
+    end
+end 
+
+
+class While
+    attr_accessor :bool_logic, :states
+    def initialize(bool_logic, states)
+        @bool_logic = bool_logic
+        @states = states
+    end
+    def eval()
+        check_stop = false
+	    while @bool_logic.eval
+            @states.each { |segment|
+            if (segment.eval() == "stop")
+                check_stop = true
+            end }
+            if (check_stop == true)
+                break
+            end
+        end
+        @states
+    end
+end
+
+class Stop
+    def initialize()
+    end
+    def eval()
+        return "stop"
+    end
+end
+
+class Function     
+    attr_accessor :def_name, :f_arguments, :states
+    def initialize(def_name, f_arguments, states)
+        @def_name = def_name
+        @f_arguments = f_arguments
+        @states = states
+        if !$our_funcs.has_key?(@def_name)
+            $our_funcs[def_name] = self
+        else
+            raise("OOOPS! THE FUNCTION \"#{@def_name}\" DOES ALREADY EXIST!")
+        end
+    end
+    def recieveStates()
+        @states
+    end
+    def recieveArgs()
+        @f_arguments
+    end
+end
+
+class FunctionCall
+    attr_accessor :def_name, :f_c_arguments
+    def initialize(def_name, f_c_arguments)
+        @def_name = def_name
+        @f_c_arguments = f_c_arguments
+        @states = $our_funcs[@def_name.variable_name].recieveStates
+        @f_arguments = $our_funcs[@def_name.variable_name].recieveArgs
+
+        if !$our_funcs.has_key?(@def_name.variable_name)
+            raise("OOOPS! THERE IS NO FUNCTION CALLED '#{@def_name.variable_name}' ")
+        end
+        if (@f_c_arguments.length != @f_arguments.length)
+            raise("FAIL! WRONG NUMBER OF ARGUMENTS. (GIVEN #{@f_c_arguments.length} EXPECTED #{@f_arguments.length})")
+        end
+    end
+    def eval()        
+        scp = $scope.incre
+        funcArgs_len = 0
+        funcCallArgs_len = @f_c_arguments.length
+        while (funcArgs_len < funcCallArgs_len)
+            scp[@f_arguments[funcArgs_len].variable_name] = @f_c_arguments[funcArgs_len].eval
+            funcArgs_len = funcArgs_len + 1
+        end   
+        @states.each { |state|
+            if state.class == Return 
+                puts "-->> Function '#{@def_name.variable_name}' returning '#{state.eval}'"
+                break
+            else
+                state.eval
+            end }
+        scp.delete($scope.receiveLevel)
+        $scope.decre(scp)
+    end
+end 
+
+class Return
+    def initialize(value)
+        @value = value
+    end
+    def eval
+        return @value.eval
+    end
+end
